30d9de2530207f67756a3e86e01947385c71c043
Turbovv <giga.elizbarashvili13@gmail.com>
2024-06-28 18:25:53 +0400

    feat: Footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main-container bg-current">
+    <div class="flex justify-center h-full">
+      <div class="relative grid grid-cols-1 md:grid-cols-2 gap-8 max-w-5xl mx-auto p-6 md:p-0 mt-64">
+        <div class="text-content relative  top-40 md:static">
+          <h1 class="text-white text-6xl lg:text-9xl uppercase font-bold mb-10 lg:mb-16">Story</h1>
+          <p class="text-white text-base lg:text-xl line-height mb-14 ">
+            On the first day of April, 3 years ago holistic <br class="hidden md:block"/> design agency Holy Motors opened for <br class="hidden md:block"/> business.
+          </p>
+          <h1 class="text-white text-lg md:text-2xl uppercase line-height font-bold mb-10 md:mb-16">
+            Holy Motors is a holistic<br class="hidden md:block"/>design agency helping <br class="hidden md:block"/> brands leave their mark on <br class="hidden md:block"/> culture.
+          </h1>
+        </div>
+        <div class="text-content">
+          <p class="text-white text-base lg:text-xl mb-10 mt-5">
+            With multidisciplinary teams of designers, <br class="hidden md:block"/> developers, strategists, writers, creators, and <br class="hidden md:block"/> artists, we work closely with founders and <br class="hidden md:block"/> brand owners through all stages of bringing <br class="hidden md:block"/> their vision to life.
+          </p>
+          <p class="text-white text-base lg:text-xl line-height">
+            As most glorious human endeavours often <br class="hidden md:block"/> are, Holy Motors was created out of nothing <br class="hidden md:block"/> when Georgia’s renowned creative duo Giorgi <br class="hidden md:block"/> Avaliani and Nick Kumbari joined their forces <br class="hidden md:block"/> to be reborn. Bringing together years of <br class="hidden lg:block"/> creative and design excellence.
+          </p>
+        </div>
+        <div class="relative bottom-32 hidden md:block sm:hidden">
+          <div class="flex justify-between items-center md:gap-16">
+            <img src="../../assets/Footer_Images/image-188.png" alt="Image 1" class="lg:w-full object-cover" />
+            <img src="../../assets/Footer_Images/image-192.png" alt="Image 2" class="lg:w-full object-cover" />
+            <img src="../../assets/Footer_Images/image-193.png" alt="Image 3" class="lg:w-full object-cover" />
+          </div>
+        </div>
+        <div class="relative bottom-20 items-center grid grid-cols-1 gap-4 sm:block md:hidden">
+          <div class="grid grid-cols-1 items-center">
+            <img src="../../assets/Footer_Images/image-188.png" alt="Image 1" class="object-cover" />
+            <img src="../../assets/Footer_Images/image-192.png" alt="Image 2" class="object-cover" />
+            <img src="../../assets/Footer_Images/image-193.png" alt="Image 3" class="object-cover" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  background-image: url('../../assets/Footer_Images/bg-image.png');
+  background-size: cover;
+  height: 100vh;
+}
+
+.line-height {
+  line-height: 1.8rem;
+}
+</style>
